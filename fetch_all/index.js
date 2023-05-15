5b5c707fca72a56c4bba4b97a8a24cc3bebd03de
Moses Ngan <mosesngan@Mosess-MacBook-Pro.local>
2023-05-15 10:57:41 +0800

    add my first lambda function fetch_all

---
+++ fetch_all/index.js
@@ -0,0 +1,33 @@
+const { Client } = require('pg');
+
+exports.handler = async (event) => {
+  const client = new Client({
+    connectionString: 'postgres://pghjywwz:2R6SujI3nBkhaG1EqdXDLgGrpumoGTyq@tiny.db.elephantsql.com/pghjywwz',
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+  await client.connect();
+  console.log('Connected to database');
+
+  try {
+    const result = await client.query('SELECT * FROM "myTable"');
+    const data = result.rows;
+    console.log('Fetched data:', data);
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+
+    return response;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
+    await client.end();
+    console.log('Disconnected from database');
+  }
+};
+
